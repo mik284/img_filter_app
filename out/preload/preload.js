8ff460274f3b5b79e703120f4cac18b5334b3f1c
mik284 <mikeykamau222@gmail.com>
2024-01-24 19:40:20 +0300

    add electron sturct

--- out/preload/preload.js
+++ out/preload/preload.js
@@ -1,1 +1,5 @@
 "use strict";
+const electron = require("electron");
+electron.contextBridge.exposeInMainWorld("electronAPI", {
+  openFile: () => electron.ipcRenderer.invoke("dialog:openFile")
+});
